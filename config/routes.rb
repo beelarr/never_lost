8e280973ed71c24826e314e63a693dc17a60e8ff
beelarr <bryonl@me.com>
2017-12-08 11:05:15 -0600

    Set up database and altered root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'trips/new'
+  root 'trips/new'
 
-  get 'trips/create'
+  resources :trips, only: [:new, :create, :show]
 
-  get 'trips/show'
-
-  get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
