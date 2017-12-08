3ba8654c43d997e2c135fd565616ecaeaf46c84a
beelarr <bryonl@me.com>
2017-12-08 11:02:41 -0600

    G trip model and controller, checkin model

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,28 @@
+class TripsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+  end
+
+  def create
+    clean_old_trips
+    trip = Trip.create!(trip_params)
+    render json: trip.to_json
+  end
+
+  def show
+    trip = Trip.find_by!(viewer_uuid: params[:id])
+    @trip_props = trip.attributes
+    @trip_props.delete(:owner_uuid)
+  end
+
+  private
+
+  def clean_old_trips
+    Trip.where('created_at < ?', 2.days.ago).delete_all
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'trips/new'
+
+  get 'trips/create'
+
+  get 'trips/show'
+
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
